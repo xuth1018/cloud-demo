0b8fffc1b069a503f44a18b42be62ac6622b835a
xth <2624235265@qq.com>
2020-05-12 10:58:52 +0800

    21

--- cloud-demo/config-server/src/main/java/com/xx/cloud/ConfigServerApp.java
+++ cloud-demo/config-server/src/main/java/com/xx/cloud/ConfigServerApp.java
@@ -1,4 +1,11 @@
 package com.xx.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConfigServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class,args);
+    }
 }
