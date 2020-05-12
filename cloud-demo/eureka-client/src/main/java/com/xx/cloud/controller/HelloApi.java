bb38880fe68d43539ad96c71804b7ac96f6c0230
xth <2624235265@qq.com>
2020-05-13 00:24:41 +0800

    '123'

--- cloud-demo/eureka-client/src/main/java/com/xx/cloud/controller/HelloApi.java
+++ cloud-demo/eureka-client/src/main/java/com/xx/cloud/controller/HelloApi.java
@@ -3,19 +3,25 @@ package com.xx.cloud.controller;
 
 import com.xx.cloud.config.ServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
+@RefreshScope
 public class HelloApi {
 
     @Autowired
     private ServerConfig serverConfig;
 
+    @Value("${cloud.site}")
+    private String site;
     @GetMapping("/hello")
     public String hello(){
+        System.out.println(site);
         return serverConfig.getServerPort()+":hello";
     }
 }
